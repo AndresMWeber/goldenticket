17857fff851cb9ad9b7f5cad662893fcb3cdf79b
Glitch (hello-express) <none>
2021-03-25 14:58:30 +0000

    👗🏬 Checkpoint ./bot.js:30173880/371

--- bot.js
+++ bot.js
@@ -1,10 +1,14 @@
 const Discord = require("discord.js");
 
 const client = new Discord.Client();
+const newUsers = new Discord.Collection();
+let activeUsers = [];
 
-
-client.on("message", function(message) { 
-                                         
+client.on("guildMemberAdd", function(message) { 
+  const list = client.guilds.cache.get("myServerID");
+  activeUsers = list.members.cache.forEach(member => member.user);                               
 });                                      
 
 client.login(process.env.BOT_TOKEN);
+
+export default activeUsers
